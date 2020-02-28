ce45b602407b7e0e71d9422d6734829f2e687e99
tram0808 <phanquynhbichtram88@gmail.com>
2020-02-28 18:23:06 +0700

    kieu du lieu: object

--- buoi1.js
+++ buoi1.js
@@ -1,4 +1,15 @@
-var a=10
-a="hello"
+//1 :khai bao bien
+//var x= 5
+//var y=8
+//console.log(x)
 
-console.log(a)
+// 2.kieu du lieu
+//cac truong hop gap undefined
+// th1: khai báo không truyền giá trị
+// var let không khai báo thì undefined nhưng const thì ko đc
+ //const a
+ //console.log(a)
+//th2: gọi thuộc tính ko tồn tại trong object
+ //3: object
+// var teo={name:"Nguyen Van Teo",age:10}
+ //console.log(teo.address)
